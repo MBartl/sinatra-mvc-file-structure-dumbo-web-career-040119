ff4e47fb99e8566e3740d9108858dc92304c0c41
Matthew Bartl <mattbartl@gmail.com>
2019-04-23 17:18:26 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,11 @@
-require 'config/environment.rb'
-
 class ApplicationController < Sinatra::Base
-  
-  @dog = Dog.all
 
   configure do
-  	set :views, "app/views"
-  	set :public_dir, "public"
+    set :views, "app/views"
+    set :public_dir, "public"
   end
 
-  get "/" do
-  	erb :index
+  get '/' do
+    erb :index
   end
-  
 end
